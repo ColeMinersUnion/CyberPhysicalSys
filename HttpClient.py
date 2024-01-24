8c769c9fee9d69fb6fae0c3acacf54014ed0fff5
ColeMinersUnion <colehansen018@gmail.com>
2024-01-24 17:18:22 -0500

    Did something probably

---
+++ HttpClient.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+#this means that I can create a website with clues through a GitHub Pages
+uri = "https://coleminersunion.github.io" #THe uri that I'm accessing
+
+session = requests.Session() #initializing a requests object.
+response = session.get(uri) #using a GET method on the URI. I didn't edit my headers. 
+
+html_soup = BeautifulSoup(response.text, 'html.parser') #i like my things readable.
+print(html_soup) #seeing the html
--- HttpServer.py
+++ HttpServer.py
@@ -1,1 +1,9 @@
 import http.server
+import socketserver
+
+PORT = 8000
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("10.5.124.47", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever() #this works, but only on my own machine
